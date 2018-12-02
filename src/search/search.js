9cdfae78fcbd478e765bbafa4601174c1a61e594
Siobhan Williams <vonnie2k5@gmail.com>
2018-12-01 22:02:24 -0500

    add: Add backend for twitter calls

--- src/search/search.js
+++ src/search/search.js
@@ -1,14 +1,13 @@
-var Twitter = require('twitter');
+const Twitter = require('twitter');
 
-
-var client = new Twitter({
-  consumer_key: '3FBvhdWzCyojUJ6i6WnUp0loI',
-  consumer_secret: 'AA8xYoatm7mhSp5rSwts7sgLj0mSt8wTRF4cM3HWG16SfJp6SO',
-  access_token_key: '1068990524370100224-Vi2YWgq0Q8OHjf1qRvxxVVVuCXUVhd',
-  access_token_secret: 'xiQsO0AlbMAOf19JpaYGMlGThkHBkPgNTQrzHU0G6ddrH'
+const client = new Twitter({
+ consumer_key: '3FBvhdWzCyojUJ6i6WnUp0loI',
+ consumer_secret: 'AA8xYoatm7mhSp5rSwts7sgLj0mSt8wTRF4cM3HWG16SfJp6SO',
+ access_token_key: '1068990524370100224-Vi2YWgq0Q8OHjf1qRvxxVVVuCXUVhd',
+ access_token_secret: 'xiQsO0AlbMAOf19JpaYGMlGThkHBkPgNTQrzHU0G6ddrH'
 });
 
+module.exports = function (search) {
+  return client.get('search/tweets', {q: search});
+};
 
-client.get('search/tweets', {q: 'test'}, function(error, tweets, response) {
-   console.log(tweets);
-});
